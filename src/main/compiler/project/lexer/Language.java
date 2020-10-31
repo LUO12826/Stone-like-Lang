c17a0656ad9b43d61fc62a120efa014c329424ba
LUO12826 <luohuizhou@hotmail.com>
2020-10-31 16:08:15 +0800

    增加一些基本结构

---
+++ src/main/compiler/project/lexer/Language.java
@@ -0,0 +1,9 @@
+package compiler.project.lexer;
+
+/**
+ * @author: 骆荟州
+ * @createTime: 2020/10/31 3:02 下午
+ * @updateTime:
+ */
+public class Language {
+}
---
+++ src/main/compiler/project/lexer/Lexer.java
@@ -0,0 +1,9 @@
+package compiler.project.lexer;
+
+/**
+ * @author: 骆荟州
+ * @createTime: 2020/10/31 2:58 下午
+ * @updateTime:
+ */
+public class Lexer {
+}
