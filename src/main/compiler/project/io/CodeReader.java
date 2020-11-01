0b36dd5cf75b971eb34fcebff6567e37dca11bf2
LUO12826 <luohuizhou@hotmail.com>
2020-11-01 11:40:40 +0800

    L-修复CodeReader中一些潜在的问题；为CodeReader增加获取当前行列的功能；为Token增加toString方法。

--- src/main/compiler/project/io/CodeReader.java
+++ src/main/compiler/project/io/CodeReader.java
@@ -1,8 +1,9 @@
 package compiler.project.io;
 
 import org.apache.commons.io.FileUtils;
-
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: 骆荟州
@@ -17,6 +18,15 @@ public class CodeReader {
 
     private int codeLength = 0;
 
+    private int line = 1;
+
+    private int col = 0;
+
+    private Map<Integer, Integer> colNumMap = new HashMap<>();
+
+
+    private boolean lineEnd = false;
+
     public CodeReader(File file) throws Exception {
         this.codeString = FileUtils.readFileToString(file, "UTF-8");
         this.codeLength = codeString.length();
@@ -28,20 +38,51 @@ public class CodeReader {
     }
 
     public char nextChar() {
-        if(currentIndex >= codeLength - 1) {
+        if(currentIndex < codeLength - 1) {
+            char c = codeString.charAt(++currentIndex);
+            if(c == '\n') {
+                lineEnd = true;
+                col++;
+            }
+            if(lineEnd) {
+                lineEnd = false;
+                colNumMap.put(line, col);
+                line++;
+                col = 1;
+            }
+            else {
+                col++;
+            }
+            return c;
+        }
+        else if(currentIndex == codeLength - 1) {
             currentIndex++;
             return '\0';
         }
-        return codeString.charAt(++currentIndex);
+        else {
+            return '\0';
+        }
     }
 
     public char retract() {
-        if(currentIndex <= 0) {
+        if(currentIndex >= 0) {
+            currentIndex--;
+            char c = currentIndex == -1? codeString.charAt(0) : codeString.charAt(currentIndex);
+            if(c == '\n' && currentIndex + 1 != codeLength) {
+                lineEnd = true;
+                line--;
+                col = colNumMap.get(line);
+            }
+            else {
+                col--;
+            }
+            return c;
+        }
+        else {
             currentIndex = -1;
+            col = -1;
             return codeString.charAt(0);
         }
-        currentIndex--;
-        return codeString.charAt(currentIndex);
     }
 
     public char nextNotSpaceChar() {
@@ -59,5 +100,19 @@ public class CodeReader {
 
     public char peek() {
         return currentIndex >= codeLength - 1? '\0' : codeString.charAt(currentIndex + 1);
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void reset() {
+        currentIndex = -1;
+        line = 1;
+        col = 0;
     }
 }
